7934d2fb68762de84c9d352fe491b50a74a26102
Quentin Gans <quentin.gans@outlook.fr>
2018-11-25 21:01:00 +0100

    final update

--- GANS_Quentin_kholle_1.py
+++ GANS_Quentin_kholle_1.py
@@ -1,5 +1,9 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6
 
+# Title : Kholle 1.5
+# Description : utility for a work of simple operations, on which it will be written in a file in csv format.
+# Last update : 25/11/18
+# Author : Gans Quentin
 
 # Imports
 import argparse
@@ -30,12 +34,20 @@ def write_in_file(msg):
         write.writerow(msg)
 
 
+# Create listeVals.csv
+def create_file():
+    with open(csv_file, "w") as new_file:
+        csv.writer(new_file)
+        new_file.close()
+    print('The file has just been created !')
+
+
 # Suppr all items in list
 def suppr_all_items():
     while lengthListe != 0:
         listeVals.remove([0])
     write_in_file(listeVals)
-    print('Toutes les valeurs sont effaces !')
+    print('All values ​​are cleared !')
 
 
 # Get max value
@@ -58,72 +70,77 @@ def min_val(arr):
 
 # Args
 parser = argparse.ArgumentParser(conflict_handler='resolve')
-parser.add_argument("-l", action="store_true", help="Affiche le contenu de la liste")
-parser.add_argument("-a", nargs="*", help="Ajoute les items dans la liste")
-parser.add_argument("-c", action="store_true", help="Supprime tous les éléments de la liste")
-parser.add_argument("-s", "--max", action="store_true", help="Affiche la valeur maximum contenu dans la liste")
-parser.add_argument("-s", "--min", action="store_true", help="Affiche la valeur minimum contenu dans la liste")
-parser.add_argument("-s", "--moy", action="store_true", help="Affiche la moyenne de tous les éléments dans la liste.")
-parser.add_argument("-s", "--sum", action="store_true", help="Affiche la somme de tous les éléments dans la liste.")
-parser.add_argument("-t", action="store_true", help="Trie la liste dans l’ordre croissant.")
-parser.add_argument("--desc", action="store_true", help="Trie la liste dans l’ordre decroissant.")
-parser.add_argument("--help", action="store_true", help="Affiche le manuel d’utilisation de cette commande.")
+parser.add_argument("-l", action="store_true", help="Display the contents of the list.")
+parser.add_argument("-a", nargs="*", help="Add the items to the list.")
+parser.add_argument("-c", action="store_true", help="Removes all items from the list.")
+parser.add_argument("-s", "--max", action="store_true", help="Displays the maximum value in the list.")
+parser.add_argument("-s", "--min", action="store_true", help="Displays the minimum value in the list.")
+parser.add_argument("-s", "--moy", action="store_true", help="Shows the average of all items in the list.")
+parser.add_argument("-s", "--sum", action="store_true", help="Displays the sum of all items in the list.")
+parser.add_argument("-t", action="store_true", help="Sort the list in ascending order.")
+parser.add_argument("--desc", action="store_true", help="Sort the list in descending order.")
+parser.add_argument("--help", action="store_true", help="Displays the user manual of this command.")
 args = parser.parse_args()
 
 # Program
 
-if args.l:
-    read_in_file()
-    for nbr in listeVals:
-        print(nbr)
+try:
 
-elif args.a:
-    read_in_file()
-    for val in args.a:
-        if val.isdigit():
-            listeVals.append(val)
-            write_in_file(listeVals)
-            print('Valeur', val, 'ajoute')
-        else:
-            print('Entrez un nombre entier svp')
-            exit()
+    if args.l:
+        read_in_file()
+        for nbr in listeVals:
+            print(nbr)
 
-elif args.c:
-    suppr_all_items()
+    elif args.a:
+        read_in_file()
+        for val in args.a:
+            if val.isdigit():
+                listeVals.append(val)
+                write_in_file(listeVals)
+                print('Value', val, 'added')
+            else:
+                print('Please enter an integer')
+                exit()
 
-elif args.max:
-    read_in_file()
-    rows = [int(row) for row in listeVals]
-    highest = max_val(rows)
-    print('La plus grosse valeur est', highest)
+    elif args.c:
+        suppr_all_items()
 
-elif args.min:
-    read_in_file()
-    rows = [int(row) for row in listeVals]
-    lowest = min_val(rows)
-    print('La plus petite valeur est', lowest)
+    elif args.max:
+        read_in_file()
+        rows = [int(row) for row in listeVals]
+        highest = max_val(rows)
+        print('The biggest value is', highest)
 
-elif args.moy:
-    read_in_file()
-    rows = [int(row) for row in listeVals]
-    moy = (sum(rows) / len(listeVals))
-    print('La moyenne des valeurs est de', moy)
+    elif args.min:
+        read_in_file()
+        rows = [int(row) for row in listeVals]
+        lowest = min_val(rows)
+        print('The smallest value is', lowest)
 
-elif args.sum:
-    read_in_file()
-    rows = [int(row) for row in listeVals]
-    total = sum(rows)
-    print('La somme total est de', total)
+    elif args.moy:
+        read_in_file()
+        rows = [int(row) for row in listeVals]
+        moy = (sum(rows) / len(listeVals))
+        print('The average value is', moy)
 
-elif args.t:
-    read_in_file()
-    rows = [int(row) for row in sorted(listeVals, key=int)]
-    print('Trie par ordre croissant:', rows)
+    elif args.sum:
+        read_in_file()
+        rows = [int(row) for row in listeVals]
+        total = sum(rows)
+        print('The total sum is', total)
 
-elif args.desc:
-    read_in_file()
-    rows = [int(row) for row in sorted(listeVals, key=int, reverse=True)]
-    print('Trie par ordre decroissant:', rows)
+    elif args.t:
+        read_in_file()
+        rows = [int(row) for row in sorted(listeVals, key=int, reverse=False)]
+        print('Sort in ascending order:', rows)
 
-else:
-    parser.print_help()
+    elif args.desc:
+        read_in_file()
+        rows = [int(row) for row in sorted(listeVals, key=int, reverse=True)]
+        print('Sort in descending order:', rows)
+
+    else:
+        parser.print_help()
+
+except FileNotFoundError:
+    create_file()
